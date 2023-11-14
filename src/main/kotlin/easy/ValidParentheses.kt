3254aa9cefae5aeac14fb6e3721b8053178b58e4
kimjh <kimjh@xxblue.com>
2023-11-14 18:12:23 +0900

    20. Valid Parentheses 풀이

---
+++ src/main/kotlin/easy/ValidParentheses.kt
@@ -0,0 +1,28 @@
+package easy
+
+import java.util.*
+
+class ValidParentheses {
+    fun isValid(s: String): Boolean {
+        if(s.isEmpty() || s.length == 1) return false
+
+        val stack = Stack<Char>()
+
+        for (c in s) {
+            when (c) {
+                '(', '{', '[' -> stack.push(c)
+                ')' -> if(stack.isEmpty() || stack.pop() != '(') return false
+                '}' -> if(stack.isEmpty() || stack.pop() != '{') return false
+                ']' -> if(stack.isEmpty() || stack.pop() != '[') return false
+            }
+        }
+
+        return stack.isEmpty()
+    }
+
+}
+
+fun main() {
+    val solution = ValidParentheses().isValid("(])")
+    println(solution)
+}
